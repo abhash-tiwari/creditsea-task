6b8ae56c493868cf6d1880661c8c4225387bd056
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-08 00:20:01 +0530

    Project Structure

---
+++ frontend/src/components/CreditAccounts/CreditAccounts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreditAccounts = () => {
+  return (
+    <div>CreditAccounts</div>
+  )
+}
+
+export default CreditAccounts
---
+++ frontend/src/components/ReportSummary/ReportSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ReportSummary = () => {
+  return (
+    <div>ReportSummary</div>
+  )
+}
+
+export default ReportSummary
