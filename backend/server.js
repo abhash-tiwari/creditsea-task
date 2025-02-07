22ea7c5109b09631e416fda025f1d149f1bb66fa
Abhash Tiwari <abhashhtiwary7610@gmail.com>
2025-02-07 16:42:43 +0530

    MongoDB Connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
 require('dotenv').config();
 
@@ -8,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
